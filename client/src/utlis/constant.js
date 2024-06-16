8d62fa7a081cf6b95c0c43086111817526915e07
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2024-06-16 18:13:53 +0530

    firebase remove 2

--- client/src/utlis/constant.js
+++ client/src/utlis/constant.js
@@ -6,7 +6,7 @@ export const CDN_URL =
 export const MENU_API = "https://foody-pearl-three.vercel.app/api/menu"; // Assuming your proxy server runs on localhost:3001 and has a route for menu data
 
 
-export let CORS_PROXY_URL_ = "https://foody-pearl-three.vercel.app/";
+export let CORS_PROXY_URL_ = "https://foody-pearl-three.vercel.app";
 export const getListAPI = (latitude, longitude) => {
 //   const timestamp = Date.now();
   return `${CORS_PROXY_URL_}/api/res?lat=${latitude}&lng=${longitude}`;
